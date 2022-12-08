47775a030a69669d04665c77ce76b0b5a9902ef6
MalikaBoss <malika.boss@kcl.ac.uk>
2022-12-08 19:54:42 +0000

    fix errors

--- primes.py
+++ primes.py
@@ -1,8 +1,21 @@
+import math
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
     list = []
-    while len(list) < number_of_primes + 1:
-        if 
+    i = 2
+    if number_of_primes < 0 or number_of_primes == 0:
+        raise ValueError
+    while len(list) < number_of_primes:
+        if is_prime(i):
+            list.append(i)
+        i += 1
     return list
+
+def is_prime(num):
+    for i in range(2,num):
+        if(num%i == 0):
+            return False
+    return True
+
